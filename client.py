81280d1449c99b834bd1ffd74bcc1de54de62a08
imrahulkant <rahulkant1000@gmail.com>
2021-05-18 10:42:30 +0530

    master

--- client.py
+++ client.py
@@ -1,12 +1,21 @@
 import socket
 
 socketServer = socket.socket()
-sip = input("Enter your ip : ")
+sip = input("Enter Server ip : ")
 sport = 8334
+
+name = input('Enter Your name: ')
+
 socketServer.connect((sip, sport))
+
+socketServer.send(name.encode())
+server_name = socketServer.recv(1024)
+server_name = server_name.decode()
+ 
+print(server_name,' has joined...')
 
 while True:
     message = (socketServer.recv(1024)).decode()
-    print("server_name", ":", message)
+    print(server_name, ":", message)
     message = input("Me : ")
     socketServer.send(message.encode())
--- server.py
+++ server.py
@@ -1,16 +1,29 @@
 import socket
 
 newSocket = socket.socket()
-sip = input("Enter your ip : ")
+sip = input("Enter Client ip : ")
 port = 8334
 newSocket.bind((sip, port))
+
+print( "Binding successful!" )
+print("This is your IP: ", sip)
+
+name = input('Enter Your name: ')
+
 newSocket.listen(1) 
 conn, add = newSocket.accept()
 
- 
+print("Received connection from ", add[0])
+print('Connection Established. Connected From: ',add[0])
+
+client = (conn.recv(1024)).decode()
+print(client + ' is Online . . .')
+
+conn.send(name.encode())
+
 while True:
     message = input('Me : ')
     conn.send(message.encode())
     message = conn.recv(1024)
     message = message.decode()
-    print("client", ':', message)
+    print(client, ':', message)
