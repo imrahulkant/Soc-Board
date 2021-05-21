c7f4fa7e34f01dafd6a045a68c32aee950ff6538
imrahulkant <rahulkant1000@gmail.com>
2021-05-21 10:16:51 +0530

    added formats

--- clientclipboard.py
+++ clientclipboard.py
@@ -1,11 +1,21 @@
 import pyperclip as pc
 import socket
+from rich import print
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sip = input("\nEnter Server ip : ")
 sport = 8889
 
+host_name = socket.gethostname()
+ip = socket.gethostbyname(host_name)
+
+print( "\n\nBinding successful!" )
+print("This is Client IP: ", sip)
+print("This is Your IP: ", ip)
+
 server.connect((sip,sport))
+
+print( "\n\nClipboard Not Shared Yet .... \n" )
 
 while True:
     data = "No data in clipboard"
@@ -14,3 +24,4 @@ while True:
     else:
         data = pc.waitForNewPaste()
     server.send(data.encode())
+    print("\t [blue] Clipboard Shared... [/blue] \n")
--- serverclipboard.py
+++ serverclipboard.py
@@ -1,17 +1,29 @@
-import pyperclip as pc
 import socket
+from rich import print
+from rich.panel import Panel
+from rich.text import Text
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 cip = input("\nEnter Client ip : ")
 port = 8889
 client.bind((cip, port))
+host_name = socket.gethostname()
+ip = socket.gethostbyname(host_name)
+ 
 
+print( "\n\nBinding successful!" )
+print("This is Client IP: ", cip)
+print("This is Your IP: ", ip)
 
 client.listen(1) 
 conn, add = client.accept()
 
+print("\n[bold magenta][*]OK. Clipboard is now shared. Wait... [/bold magenta]\n")
+
 while True:
+
     message = conn.recv(1024)
     message = message.decode()
-    print("\nData Recived...")
-    print("\n\t {}".format(message))
+    panel = Text(message, justify="left")
+    print("\n [green] Data Recived... [green]")
+    print("\n\t {}".format(panel))
